05d19453e7ed6f0af814eb4f18b9ecf04715c771
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2020-11-06 21:08:54 -0600

    Crewmates added and minimap is actually mini now

--- Proyecto/RayCaster.py
+++ Proyecto/RayCaster.py
@@ -19,6 +19,32 @@ enemies = [
     "y": 75,
     "texture": pygame.image.load('textures/red.png')
   },
+  {
+   "x": 500,
+    "y": 125,
+    "texture": pygame.image.load('textures/orange.png')
+  },
+  {
+   "x": 850,
+    "y": 75,
+    "texture": pygame.image.load('textures/purple.png')
+  },
+  {
+   "x": 900,
+    "y": 325,
+    "texture": pygame.image.load('textures/black.png')
+  },
+  {
+   "x": 400,
+    "y": 625,
+    "texture": pygame.image.load('textures/blue.png')
+  },
+  {
+   "x": 550,
+    "y": 375,
+    "texture": pygame.image.load('textures/cyan.png')
+  },
+  
 ]
 
 textures = {
@@ -55,8 +81,8 @@ class Raycaster:
         screen.set_at((x, y), c)
 
     def draw_rectangle(self, x, y, texture):
-        for cx in range(x, x + 25):
-            for cy in range(y, y + 25):
+        for cx in range(x, x + 12):
+            for cy in range(y, y + 12):
                 tx = int((cx - x) * block_aspect_ratio_width) 
                 ty = int((cy - y) * block_aspect_ratio_height)
                 c = texture.get_at((tx, ty))
@@ -148,20 +174,21 @@ class Raycaster:
             self.draw_stake(x, h, textures[c], tx)
             self.zbuffer[i] = d
 
-        for x in range(0, 500,25):
-            for y in range(0, 325,25):
-                i = int(x/25)
-                j = int(y/25)
-                if self.map[j][i] != ' ' and self.map[j][i] != '\n': 
-                    self.draw_rectangle(x, y, textures[self.map[j][i]])
-                else:
-                    self.point(i,j,(255,255,255))
         
         for enemy in enemies:
             self.draw_sprite(enemy)
         
-        # self.draw_healthbar(800, 0)
-        # self.draw_HUD(0,400)
+        for x in range(0,240):
+            for y in range(0,150):
+                self.point(x,y,(51,255,51))
+        for x in range(0, 250,12):
+            for y in range(0, 162,12):
+                i = int(x/12)
+                j = int(y/12)
+                if self.map[j][i] != ' ' and self.map[j][i] != '\n': 
+                    self.draw_rectangle(x, y, textures[self.map[j][i]])
+                
+                    
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
